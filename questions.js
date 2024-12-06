e5fc69c002fcd9515347445a61ebd0526c523f94
laura <20555932g@iespoblenou.org>
2024-12-06 01:20:42 +0100

    added all images in questions.js

--- questions.js
+++ questions.js
@@ -14,7 +14,7 @@ export default {
       ],
     },
     {
-      questionType: 'text',
+      questionType: 'image',
       answerType: 'text',
       question: 'Quin tipus és Pikachu?',
       image:
@@ -23,17 +23,17 @@ export default {
       options: ['Aigua', 'Foc', 'Normal'],
     },
     {
-      questionType: 'text',
+      questionType: 'image',
       answerType: 'text',
       question: 'En quina regió comença Ash la seva aventura en la sèrie?',
       image:
-        'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/5/5b/latest/20180712231931/Mapa_de_Kanto_LGPE.png/428px-Mapa_de_Kanto_LGPE.png',
+        'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/6/62/latest/20150428172738/Ash_Ketchum_%28anime_original%29.png/220px-Ash_Ketchum_%28anime_original%29.png',
       correct: 'Kanto',
       options: ['Johto', 'Sinnoh', 'Hoenn'],
     },
     {
       questionType: 'text',
-      answerType: 'text',
+      answerType: 'image',
       question: "Quin objecte s'usa per a atrapar Pokémon?",
       correct:
         'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/0/02/latest/20231226202856/Pok%C3%A9_Ball_%28Ilustraci%C3%B3n%29.png/180px-Pok%C3%A9_Ball_%28Ilustraci%C3%B3n%29.png',
@@ -45,44 +45,51 @@ export default {
     },
     {
       questionType: 'text',
-      answerType: 'text',
+      answerType: 'image',
       question: "Què Pokémon té l'habilitat de parlar en la sèrie?",
-      correct: 'Meowth',
-      options: ['Jigglypuff', 'Psyduck', 'Pikachu'],
+      correct: 'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/9/99/latest/20160904210550/Meowth.png/300px-Meowth.png',
+      options: [
+        'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/a/af/latest/20150110232910/Jigglypuff.png/300px-Jigglypuff.png', 
+        'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/3/32/latest/20230614194705/Psyduck.png/300px-Psyduck.png', 
+        'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/7/77/latest/20150621181250/Pikachu.png/300px-Pikachu.png'
+      ],
     },
     {
       questionType: 'text',
       answerType: 'text',
-      question: "Quin és el nom de l'equip vilà a la regió de Kanto?",
+      question: "Quin és el nom de l'equip malvat a la regió de Kanto?",
       correct: 'Team Rocket',
       options: ['Team Magma', 'Team Aqua', 'Team Galàxia'],
     },
     {
-      questionType: 'text',
+      questionType: 'image',
       answerType: 'text',
       question: 'Quants Pokémon hi havia originalment en la primera generació?',
+      image: 'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/b/b7/latest/20101009152426/Pok%C3%A9dex_GenI_Kanto.png/285px-Pok%C3%A9dex_GenI_Kanto.png',
       correct: '151',
       options: ['100', '200', '120'],
     },
     {
-      questionType: 'text',
+      questionType: 'image',
       answerType: 'text',
       question: 'Quin tipus és Bulbasaur?',
+      image: 'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/4/43/latest/20190406170624/Bulbasaur.png/300px-Bulbasaur.png',
       correct: 'Planta/Verí',
       options: ['Aigua/Fantasma', 'Planta/Psíquic', 'Verí/Sinistre'],
     },
     {
-      questionType: 'text',
+      questionType: 'image',
       answerType: 'text',
       question: "Com es diu l'evolució de Charmander?",
+      image: 'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/f/fb/latest/20200411222755/Charmeleon.png/300px-Charmeleon.png',
       correct: 'Charmeleon',
       options: ['Charizard', 'Blaziken', 'Magmar'],
     },
     {
-      questionType: 'text',
+      questionType: 'image',
       answerType: 'text',
       question:
-        'En quina consola es va llançar per primera vegada Pokémon Rojo i Blava?',
+        'En quina consola es va llançar per primera vegada Pokémon Vermell i Blau?',
       image:
         'https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2019/02/Pokemon-Red-and-Blue-main.jpg?tf=1200x',
       correct: 'Game Boy',
@@ -91,7 +98,7 @@ export default {
   ],
   normal: [
     {
-      questionType: 'text',
+      questionType: 'image',
       answerType: 'text',
       question: "Com es diu l'evolució d'Eevee de tipus Aigua?",
       image:
@@ -100,22 +107,27 @@ export default {
       options: ['Jolteon', 'Flareon', 'Leafeon'],
     },
     {
-      questionType: 'text',
+      questionType: 'image',
       answerType: 'text',
       question: 'Quin tipus és resistent al tipus Elèctric?',
+      image: 'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/a/a2/latest/20230317204740/Energ%C3%ADa_Rayo_B%C3%A1sica_%28Escarlata_y_P%C3%BArpura_TCG%29.png/375px-Energ%C3%ADa_Rayo_B%C3%A1sica_%28Escarlata_y_P%C3%BArpura_TCG%29.png',
       correct: 'Terra',
       options: ['Aigua', 'Foc', 'Gel'],
     },
     {
       questionType: 'text',
-      answerType: 'text',
+      answerType: 'image',
       question:
         'Quin és el Pokémon llegendari de la regió de Johto associat amb les mars?',
-      correct: 'Lugia',
-      options: ['Ho-Oh', 'Suicune', 'Entei'],
+      correct: 'https://images.wikidexcdn.net/mwuploads/wikidex/a/a7/Lugia.png',
+      options: [
+        'https://images.wikidexcdn.net/mwuploads/wikidex/0/00/Ho-Oh.png', 
+        'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/c/c3/latest/20141113215227/Suicune.png/300px-Suicune.png', 
+        'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/0/0a/latest/20200428203743/Entei.png/300px-Entei.png'
+      ],
     },
     {
-      questionType: 'text',
+      questionType: 'image',
       answerType: 'text',
       question: 'Com es diu el moviment exclusiu de Pikachu en Pokémon Groc?',
       image:
@@ -124,55 +136,68 @@ export default {
       options: ['Llamp', 'Espurna', 'Tro'],
     },
     {
-      questionType: 'text',
+      questionType: 'image',
       answerType: 'text',
       question: 'Què Pokémon té el número 150 en la Pokédex Nacional?',
+      image: 'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/b/b7/latest/20101009152426/Pok%C3%A9dex_GenI_Kanto.png/285px-Pok%C3%A9dex_GenI_Kanto.png',
       correct: 'Mewtwo',
       options: ['Mew', 'Articuno', 'Zapdos'],
     },
     {
       questionType: 'text',
-      answerType: 'text',
+      answerType: 'image',
       question: 'Què Pokémon inicial és de tipus Aigua a la regió de Hoenn?',
-      correct: 'Mudkip',
-      options: ['Totodile', 'Squirtle', 'Froakie'],
+      correct: 'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/a/ae/latest/20230706064339/Mudkip.png/300px-Mudkip.png',
+      options: [
+        'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/e/e7/latest/20230523210701/Totodile.png/300px-Totodile.png', 
+        'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/e/e3/latest/20160309230820/Squirtle.png/300px-Squirtle.png', 
+        'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/8/8e/latest/20230701044338/Froakie.png/300px-Froakie.png'
+      ],
     },
     {
-      questionType: 'text',
+      questionType: 'image',
       answerType: 'text',
       question: "Com es diu l'evolució final de Ralts?",
+      image: 'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/e/e8/latest/20141214185208/Ralts.png/300px-Ralts.png',
       correct: 'Gardevoir',
       options: ['Kirlia', 'Gallade', 'Gothitelle'],
     },
     {
       questionType: 'text',
-      answerType: 'text',
+      answerType: 'image',
       question: 'Quin és el Pokémon mític de la primera generació?',
-      correct: 'Mew',
-      options: ['Articuno', 'Moltres', 'Zapdos'],
+      correct: 'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/b/bf/latest/20160311010530/Mew.png/300px-Mew.png',
+      options: [
+        'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/5/52/latest/20160316192008/Articuno.png/300px-Articuno.png', 
+        'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/0/01/latest/20160316201747/Moltres.png/300px-Moltres.png', 
+        'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/d/d8/latest/20160316194916/Zapdos.png/300px-Zapdos.png'
+      ],
     },
     {
-      questionType: 'text',
+      questionType: 'image',
       answerType: 'text',
       question: 'Quin tipus és feble contra el tipus Drac?',
+      image: 'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/4/4d/latest/20120121025004/TCG_Tipo_Drag%C3%B3n.png/270px-TCG_Tipo_Drag%C3%B3n.png',
       correct: 'Drac',
       options: ['Fada', 'Gel', 'Fantasma'],
     },
     {
-      questionType: 'text',
+      questionType: 'image',
       answerType: 'text',
       question:
         'Quina habilitat té Ditto que li permet transformar-se en uns altres Pokémon?',
+      image: 'https://images.wikidexcdn.net/mwuploads/wikidex/0/03/Ditto.png',
       correct: 'Transformació',
       options: ['Imitació', 'Impostor', 'Metamorfosi'],
     },
   ],
   hard: [
     {
-      questionType: 'text',
+      questionType: 'image',
       answerType: 'text',
       question:
         'Com es diu el primer Pokémon creat, segons els desenvolupadors de Game Freak?',
+      image: 'https://static.wikia.nocookie.net/nintendo/images/0/0d/Game_freak_logo.png/revision/latest?cb=20210204034327&path-prefix=es',
       correct: 'Rhydon',
       options: ['Pikachu', 'Bulbasaur', 'Meowth'],
     },
@@ -184,30 +209,36 @@ export default {
       options: ['Snorlax', 'Slowpoke', 'Dunsparce'],
     },
     {
-      questionType: 'text',
+      questionType: 'image',
       answerType: 'text',
       question:
         'Quin és el moviment característic de Zygarde en la seva forma completa?',
+      image: 'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/8/87/latest/20160701145405/Zygarde_al_50%25.png/300px-Zygarde_al_50%25.png',
       correct: 'Càstig Central',
       options: ['Mil Fletxes', 'Mil Ones', 'Ullal Drac'],
     },
     {
-      questionType: 'text',
+      questionType: 'image',
       answerType: 'text',
       question: "Com es diu el líder de l'Equip Galàxia a la regió de Sinnoh?",
+      image: 'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/6/65/latest/20171227215721/VS_Helio_USUL.png/225px-VS_Helio_USUL.png',
       correct: 'Cyrus',
       options: ['Lysandre', 'Giovanni', 'Ghetsis'],
     },
     {
       questionType: 'text',
-      answerType: 'text',
+      answerType: 'image',
       question:
         'Què Pokémon introduït a la regió de Galar té la forma regional de tipus Fantasma?',
-      correct: 'Corsola',
-      options: ['Meowth', 'Ponyta', 'Zigzagoon'],
+      correct: 'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/6/6a/latest/20230613014701/Corsola.png/300px-Corsola.png',
+      options: [
+        'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/9/99/latest/20160904210550/Meowth.png/300px-Meowth.png', 
+        'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/5/5f/latest/20230628041337/Ponyta.png/300px-Ponyta.png', 
+        'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/a/a7/latest/20190406150949/Zigzagoon.png/300px-Zigzagoon.png'
+      ],
     },
     {
-      questionType: 'text',
+      questionType: 'image',
       answerType: 'text',
       question: 'Quantes megaevoluciones té Charizard?',
       image:
@@ -216,10 +247,11 @@ export default {
       options: ['1', '3', 'Cap'],
     },
     {
-      questionType: 'text',
+      questionType: 'image',
       answerType: 'text',
       question:
         "Què Pokémon es necessita per a activar l'esdeveniment del Regigigas en Pokémon Platí?",
+      image: 'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/f/f4/latest/20211108120853/Car%C3%A1tula_Pok%C3%A9mon_Platino_%28ESP%29.png/375px-Car%C3%A1tula_Pok%C3%A9mon_Platino_%28ESP%29.png',
       correct: 'Regice, Regirock i Registeel',
       options: [
         'Rayquaza, Groudon i Kyogre',
@@ -236,9 +268,10 @@ export default {
       options: ['Manaphy', 'Mew', 'Phione'],
     },
     {
-      questionType: 'text',
+      questionType: 'image',
       answerType: 'text',
       question: 'Quin joc va introduir el concepte de les formes Alola?',
+      image: 'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/6/66/latest/20160816232213/Raichu_de_Alola.png/300px-Raichu_de_Alola.png',
       correct: 'Pokémon Sol i Lluna',
       options: [
         'Pokémon XY',
@@ -247,9 +280,10 @@ export default {
       ],
     },
     {
-      questionType: 'text',
+      questionType: 'image',
       answerType: 'text',
       question: 'Com es diu el moviment que només aprèn Smeargle?',
+      image: 'https://images.wikidexcdn.net/mwuploads/wikidex/thumb/a/a9/latest/20230705004649/Smeargle.png/300px-Smeargle.png',
       correct: 'Esquema',
       options: ['Copiador', 'Mímica', 'Transformació'],
     },
